67bd26164b2a38cc3b117b73fbfc866fd84341b9
Py Supply <pysupply@M1-MacAir.local>
2023-06-08 12:48:25 -0400

    basic conditional and logical operators

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+print('What is your age?')
+age = int(input())
+if age >= 20 and age < 30:
+    print('You are in your twenties')
+elif age >= 30 and age < 40:
+    print('You are in your thirties')
+else:
+    print('You are not in your twenties or thirties')
+
+
+# > >= <= < == Conditionals
+# and, or, not Logical
